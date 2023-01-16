5c8ab92bd6d20de9b5baefe264bfa2ad454aaa6a
bboodd <92453279+bboodd@users.noreply.github.com>
2023-01-16 15:45:51 +0900

    lecture10

--- models/User.js
+++ models/User.js
@@ -1,4 +1,7 @@
 const mongoose = require('mongoose')
+const bcrypt = require('bcrypt')
+const saltRounds = 10
+
 
 const userSchema = mongoose.Schema({
     name: {
@@ -24,6 +27,23 @@ const userSchema = mongoose.Schema({
     },
     tokenExp: {
         type: Number
+    }
+})
+
+userSchema.pre('save', function( next ){
+    var user = this;
+
+    if(user.isModified('password')){
+        //비밀번호를 암호화 시킨다.
+        bcrypt.genSalt(saltRounds, function(err,salt){
+            if(err) return next(err)
+
+            bcrypt.hash(user.password, salt, function(err, hash){
+                if(err) return next(err)
+                user.password = hash
+                next()
+            })
+        })
     }
 })
 
