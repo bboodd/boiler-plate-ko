da27b4e90e4dd7f2629ba39af92ee150e8898015
bboodd <92453279+bboodd@users.noreply.github.com>
2023-01-22 11:45:23 +0900

    lecture22

---
+++ client/src/components/views/LandingPage/LandingPage.js
@@ -0,0 +1,18 @@
+import React,{ useEffect} from 'react'
+import axios from 'axios';
+
+function LandingPage() {
+    
+    useEffect(() => {
+        axios.get('/api/hello')
+        .then(response => console.log(response.data))
+    }, [])
+  
+    return (
+    <div>
+        LandingPage 랜딩 페이지
+    </div>
+  )
+}
+
+export default LandingPage
--- server/index.js
+++ server/index.js
@@ -22,6 +22,10 @@ mongoose.connect(config.mongoURI)
 
 app.get('/', (req, res) => res.send('Hello World~~안녕하세요 ~'))
 
+app.get('/api/hello', (req, res) => {
+    res.send("안녕하세요 ~ ")
+})
+
 app.post('/api/users/register', (req, res) => {
     //회원 가입 할때 필요한 정보들을 client에서 가져오면
     //그것들을 데이터 베이스에 넣어준다.
